eb12273e282506801c24f47186c6ecfd7f77f537
Hasantha Karunachandra <32540627+HasanthaKarunachandra@users.noreply.github.com>
2023-05-11 14:36:14 +0530

    login & singup completed

--- TheGrocery/src/com/shoto/pos/controller/LoginFormController.java
+++ TheGrocery/src/com/shoto/pos/controller/LoginFormController.java
@@ -1,4 +1,66 @@
 package com.shoto.pos.controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import com.shoto.pos.util.PasswordManager;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+import java.sql.*;
+
 public class LoginFormController {
+    public AnchorPane context;
+    public JFXTextField txtEmail;
+    public JFXPasswordField txtPassword;
+
+    public void btnSignInOnAction(ActionEvent actionEvent) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/thegrocery",
+                    "root","1234");
+            String sql = "SELECT * FROM user WHERE email=?";
+            PreparedStatement preparedStatement= connection.prepareStatement(sql);
+            preparedStatement.setString(1,txtEmail.getText());
+
+            ResultSet set= preparedStatement.executeQuery();
+            if(set.next()){
+                if(PasswordManager.checkPassword(txtPassword.getText(),set.getString("password"))){
+                    System.out.println("Completed");
+                } else {
+                    new Alert(Alert.AlertType.WARNING,"Check your password and try again!").show();
+                }
+
+            }else{
+                new Alert(Alert.AlertType.WARNING,"User email not found!").show();
+            }
+
+        } catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR,e.getMessage()).show();
+
+        }
+
+    }
+
+    private void clearFields() {
+        txtEmail.clear();
+        txtPassword.clear();
+    }
+
+    public void btnCreateAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("SignupForm");
+    }
+    private void setUi(String url) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("../view/"+url+".fxml")))
+        );
+        stage.centerOnScreen();
+    }
 }
--- TheGrocery/src/com/shoto/pos/controller/SignupFormController.java
+++ TheGrocery/src/com/shoto/pos/controller/SignupFormController.java
@@ -1,4 +1,64 @@
 package com.shoto.pos.controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import com.shoto.pos.util.PasswordManager;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class  SignupFormController {
+    public AnchorPane context;
+    public JFXTextField txtEmail;
+    public JFXPasswordField txtPassword;
+
+    public void btnRegisterNowOnAction(ActionEvent actionEvent) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/thegrocery",
+                    "add your user","add your password");
+            String sql = "INSERT INTO user VALUES(?,?)";
+            PreparedStatement preparedStatement= connection.prepareStatement(sql);
+            preparedStatement.setString(1,txtEmail.getText());
+            preparedStatement.setString(2, PasswordManager.encryptPassword(txtPassword.getText()));
+
+            if(preparedStatement.executeUpdate()>0){
+                new Alert(Alert.AlertType.CONFIRMATION,"User Saved").show();
+                clearFields();
+            }else {
+                new Alert(Alert.AlertType.WARNING,"Try Again!").show();
+            }
+
+        } catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR,e.getMessage()).show();
+
+        }
+    }
+
+    private void clearFields() {
+        txtEmail.clear();
+        txtPassword.clear();
+    }
+
+    public void btnAlreadyHaveAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("LoginForm");
+    }
+
+    private void setUi(String url) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("../view/"+url+".fxml")))
+        );
+        stage.centerOnScreen();
+    }
 }
